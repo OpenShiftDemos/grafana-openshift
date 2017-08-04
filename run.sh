2b1532134835ca55fe597b58d7426deeb109f5bd
ed <edwaado@gmail.com>
2017-08-04 23:04:49 +0000

    Modified installer to accept overrides to default locations

--- run.sh
+++ run.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
+
+source /etc/sysconfig/grafana-server
+
+[ -z "${DATAD} ] && DATAD=${DATA_DIR}
+[ -z "${PLGND} ] && PLGND=${PLUGINS_DIR}
+
 if [ ! -z "${GF_INSTALL_PLUGINS}" ]; then
   OLDIFS=$IFS
   IFS=','
   for plugin in ${GF_INSTALL_PLUGINS}; do
-    grafana-cli --pluginsDir /usr/share/grafana/data/plugins plugins install ${plugin}
+    grafana-cli --pluginsDir ${PLGND} plugins install ${plugin}
   done
   IFS=$OLDIFS
 fi
-source /etc/sysconfig/grafana-server
-cd /usr/share/grafana
+
 /usr/sbin/grafana-server \
 --config=${CONF_FILE} \
 --pidfile=${PID_FILE} \
 cfg:default.paths.logs=${LOG_DIR} \
-cfg:default.paths.data=${DATA_DIR} \
-cfg:default.paths.plugins=${PLUGINS_DIR}
+cfg:default.paths.data=${DATAD} \
+cfg:default.paths.plugins=${PLGND}
